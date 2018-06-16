7c7b8ccbdd46db195a071bb5c1d0a0910838ae87
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-06-16 16:26:27 +0600

    new

---
+++ Uri/2146_Password.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int number;
+        while(input.hasNextInt()){
+            number = input.nextInt();
+            System.out.println(number - 1);
+        }
+ 
+    }
+ 
+}
