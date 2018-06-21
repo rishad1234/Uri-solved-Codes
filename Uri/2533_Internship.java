49abb3e1cfa2531d9154e7f647f98eadd3e7eda9
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-06-21 13:52:58 +0600

    one more solved

---
+++ Uri/2533_Internship.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner input = new Scanner(System.in);
+        while(input.hasNext()){
+            int upper = 0;
+            int down = 0;
+            double result = 0;
+            int subjects = input.nextInt();
+            for(int i = 0; i < subjects; i++){
+                int grades = input.nextInt();
+                int workload = input.nextInt();
+                upper += (grades * workload);
+                down += workload;
+            }
+            result = (upper / (down * 100.00));
+            System.out.format("%.4f", result);
+            System.out.println("");
+        }
+    }
+ 
+}
